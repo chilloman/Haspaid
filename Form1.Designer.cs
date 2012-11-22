2e4fabedd2430b41650dfbf7357b4c50aeec6710
Vebjør Hallås <vh@chilloman.com>
2012-11-22 15:51:19 +0100

    Code added

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Haspaid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.btCheck = new System.Windows.Forms.Button();
+            this.lansw = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(12, 12);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(152, 20);
+            this.tbUsername.TabIndex = 0;
+            // 
+            // btCheck
+            // 
+            this.btCheck.Location = new System.Drawing.Point(12, 38);
+            this.btCheck.Name = "btCheck";
+            this.btCheck.Size = new System.Drawing.Size(152, 27);
+            this.btCheck.TabIndex = 1;
+            this.btCheck.Text = "Hapaid?";
+            this.btCheck.UseVisualStyleBackColor = true;
+            this.btCheck.Click += new System.EventHandler(this.btCheck_Click);
+            // 
+            // lansw
+            // 
+            this.lansw.AutoSize = true;
+            this.lansw.Location = new System.Drawing.Point(12, 68);
+            this.lansw.Name = "lansw";
+            this.lansw.Size = new System.Drawing.Size(0, 13);
+            this.lansw.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(182, 88);
+            this.Controls.Add(this.lansw);
+            this.Controls.Add(this.btCheck);
+            this.Controls.Add(this.tbUsername);
+            this.Name = "Form1";
+            this.Text = "Haspaid";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Button btCheck;
+        private System.Windows.Forms.Label lansw;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace Haspaid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btCheck_Click(object sender, EventArgs e)
+        {
+            string username = tbUsername.Text;
+
+            WebClient client = new WebClient();
+            string answer = client.DownloadString("https://minecraft.net/haspaid.jsp?user=" + username);
+
+            if (answer == "true")
+            {
+                lansw.Text = username + " is a paid user";
+            }
+            else
+            {
+                lansw.Text = username + " is not a paid user";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
